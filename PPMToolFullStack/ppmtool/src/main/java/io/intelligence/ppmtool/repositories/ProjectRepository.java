81e7a19f9d922aa745c5af9a2c2e7409249de614
Keval Shah <Keval0001@gmail.com>
2019-04-15 14:00:02 -0600

    FindProjectByIdentifier, Checking Id Exists or Not

--- PPMToolFullStack/ppmtool/src/main/java/io/intelligence/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/intelligence/ppmtool/repositories/ProjectRepository.java
@@ -10,5 +10,10 @@ public interface ProjectRepository extends CrudRepository<Project, Long>{
 	
 //	@Override
 //	Iterable<Project> findAllById(Iterable<Long> iterable);
+
+	Project findByProjectIdentifier(String projectId);
+	//Project findAllByid(Long id);
+	//*JPA SPECIALITY* it will automatically search based on field when new add findAllBy______ <- it can be any attribute from Project class as mentioned above;
+	
 	
 }
--- PPMToolFullStack/ppmtool/src/main/java/io/intelligence/ppmtool/services/ProjectService.java
+++ PPMToolFullStack/ppmtool/src/main/java/io/intelligence/ppmtool/services/ProjectService.java
@@ -6,25 +6,33 @@ import org.springframework.stereotype.Service;
 import io.intelligence.ppmtool.domain.Project;
 import io.intelligence.ppmtool.exceptions.ProjectIdException;
 import io.intelligence.ppmtool.repositories.ProjectRepository;
-import io.intelligence.ppmtool.web.ProjectController;
+
 
 @Service
 public class ProjectService {
 
 	@Autowired
 	private ProjectRepository projectRepository;
-	
+
 	public Project saveOrUpdate(Project project) {
-		
+
 		try {
 			project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
 			return projectRepository.save(project);
+		} catch (Exception e) {
+			throw new ProjectIdException(
+					"Project Id" + project.getProjectIdentifier().toUpperCase() + " already exists.");
 		}
-		catch(Exception e) {
-			throw new ProjectIdException("Project Id" + project.getProjectIdentifier().toUpperCase()+" already exists.");
-		}
-		
-		
 	}
 	
+	public Project findProjectByIdentifier(String projectId) {
+		
+		Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+		if(project == null) {
+			throw new ProjectIdException("Project Id " + projectId + " does not exists.");
+		}
+		
+		return project;
+	}
+
 }
